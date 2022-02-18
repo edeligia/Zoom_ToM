991d28ed3bb410980d15367dbe1e6e609d14b044
Eva <edeligia@uwo.ca>
2022-02-18 13:18:36 -0500

    false belief script updates and order file generator

--- editable/Order_generation_zoom.m
+++ editable/Order_generation_zoom.m
@@ -1,10 +1,10 @@
 number_questions_total = 10;
 number_runs = 1;
-number_participants = 2;
+number_participants = 30;
 number_conditions_total = 2;
 number_trials_total = 20;
 
-folder_output = [pwd filesep 'Orders' filesep];
+folder_output = [pwd filesep 'FB_Orders' filesep];
 if ~exist(folder_output, 'dir')
     mkdir(folder_output);
 end
@@ -22,11 +22,26 @@ for par = 1:number_participants
         conditions = condition_order;
         
         number_trials = length(questions);
-
+        number_questions = 10;
+        
+        assign_cond1 = randperm(number_questions, round(number_questions/2));
+        
+        order = [];
+        for first = [true false]
+            questions = randperm(number_questions, number_questions)';
+            
+            if first
+                conditions = arrayfun(@(x) ~any(x==assign_cond1), questions) + 1;
+            else
+                conditions = arrayfun(@(x) any(x==assign_cond1), questions) + 1;
+            end
+            
+            order = [order; questions conditions];
+        end
+        
         xls = {'Trial' 'Question' 'ConditionType'};
         xls((1:number_trials)+1,1) = num2cell((1:number_trials)');
-        xls((1:number_trials)+1,2) = num2cell(questions);
-        xls((1:number_trials)+1,3) = num2cell(conditions);
+        xls(2:21,2:3) = num2cell(order);
 
         xlswrite(sprintf('%sPAR%02d_RUN%02d.xlsx', folder_output, par, run), xls);
 
